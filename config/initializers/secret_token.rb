748f5441535cd517dce59f42303b06cd34414649
Blake Mesdag <blakemesdag@gmail.com>
2013-02-12 14:23:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GembeatServer::Application.config.secret_token = '7a70adbb439ad91d03bee6143a1b68e99ae1214764b637a51b102a7ac3ebf79fea48fcefe95d48ed575f1efcb4d99477cf83163610d10c6ac60785421446544d'
