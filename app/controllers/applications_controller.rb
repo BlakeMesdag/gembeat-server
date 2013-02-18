24fe8d46acd2ee22695d771373e7b131cc8948f0
Blake Mesdag <blakemesdag@gmail.com>
2013-02-18 01:25:10 -0500

    Adjustments to application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,17 +3,15 @@ class ApplicationsController < ApplicationController
 
   before_filter :load_application, except: [:index, :create, :new]
 
-  OPERATORS = {"=" => "=", ">" => ">", "<" => "<"}
+  OPERATORS = {"=" => "=", ">" => ">", "<" => "<", "" => "="}
 
   def index
     @applications = Application.includes(:dependencies)
 
     @applications = @applications.where(dependencies: {name: params[:gem].to_s}) unless params[:gem].blank?
-    if !params[:version].blank? && !params[:operator].present?
-      @applications = @applications.where(dependencies: {version: params[:version].to_s})
-    elsif !params[:version].blank? && params[:operator].present?
-      operator = OPERATORS["#{params[:operator]}"]
-      @applications = @applications.where("dependencies.version #{operator} ?", params[:version]) if !operator.blank?
+    if !params[:version].blank?
+      operator = OPERATORS[params[:operator].to_s]
+      @applications = @applications.where("dependencies.version #{operator} ?", params[:version].to_s) if !operator.blank?
     end
 
     @operators = [["=","="],[">",">"],["<","<"]]
