e4f32125fa03596c784ad3c0eb273a032b72ba11
Blake Mesdag <blakemesdag@gmail.com>
2013-02-23 01:02:20 -0500

    Add vulnerabilities controller and visibility for vulnerable dependencies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
     session[:redirect_to] = request.path
     redirect_to '/login' unless session[:active]
   end
+
+  def render_404
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false}
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+    end
+  end
 end
